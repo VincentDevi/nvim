237419d6632f030b114e2a2d97a85543f0a23b0a
Vincent Devilers <vincent.devilers@gmail.com>
2024-05-11 15:04:09 +0200

    added back transparancy to my theme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -5,10 +5,10 @@ return{
 	name = "catppuccin",
 	priority = 1000 ,
 
-	config = function()
+ 	config = function()
 	require("catppuccin").setup({
 		transparent_background = true
 	})
-	end,
+	end, 
 }
 
