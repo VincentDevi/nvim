c0087c59af46c8d0e4b56e114bcad4eb6aa2f33d
Vincent Devilers <vincent.devilers@gmail.com>
2024-05-11 16:06:54 +0200

    clear useless comment

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -2,18 +2,16 @@ return {
   "hrsh7th/nvim-cmp",
   event = "InsertEnter",
   dependencies = {
-    "hrsh7th/cmp-buffer", -- source for text in buffer
-    "hrsh7th/cmp-path", -- source for file system paths
+    "hrsh7th/cmp-buffer",
+    "hrsh7th/cmp-path", 
     {
       "L3MON4D3/LuaSnip",
-      -- follow latest release.
-      version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
-      -- install jsregexp (optional!).
+      version = "v2.*", 
       build = "make install_jsregexp",
     },
-    "saadparwaiz1/cmp_luasnip", -- for autocompletion
-    "rafamadriz/friendly-snippets", -- useful snippets
-    "onsails/lspkind.nvim", -- vs-code like pictograms
+    "saadparwaiz1/cmp_luasnip",
+    "rafamadriz/friendly-snippets",
+    "onsails/lspkind.nvim",
   },
   config = function()
     local cmp = require("cmp")
@@ -22,14 +20,13 @@ return {
 
     local lspkind = require("lspkind")
 
-    -- loads vscode style snippets from installed plugins (e.g. friendly-snippets)
     require("luasnip.loaders.from_vscode").lazy_load()
 
     cmp.setup({
       completion = {
         completeopt = "menu,menuone,preview,noselect",
       },
-      snippet = { -- configure how nvim-cmp interacts with snippet engine
+      snippet = { 
         expand = function(args)
           luasnip.lsp_expand(args.body)
         end,
@@ -46,15 +43,14 @@ return {
       -- sources for autocompletion
       sources = cmp.config.sources({
         { name = "nvim_lsp"},
-        { name = "luasnip" }, -- snippets
         { name = "buffer" }, -- text within current buffer
+        { name = "luasnip" }, -- snippets
         { name = "path" }, -- file system paths
       }),
 
-      -- configure lspkind for vs-code like pictograms in completion menu
       formatting = {
         format = lspkind.cmp_format({
-          maxwidth = 70,
+          maxwidth = 50,
           ellipsis_char = "...",
         }),
       },
