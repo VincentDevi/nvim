d290b1fb3467eeb6e902b957a6329f8f79d457de
Vincent Devilers <vincent.devilers@gmail.com>
2024-05-12 20:31:33 +0200

    add telescope ui to harpoon and 2 different keymap

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -9,9 +9,6 @@ return {
 		vim.keymap.set("n", "<leader>a", function()
 			harpoon:list():add()
 		end)
-		vim.keymap.set("n", "<leader>l", function()
-			harpoon.ui:toggle_quick_menu(harpoon:list())
-		end)
 		-- basic telescope configuration
 		local conf = require("telescope.config").values
 		local function toggle_telescope(harpoon_files)
@@ -31,5 +28,11 @@ return {
 				})
 				:find()
 		end
+		vim.keymap.set("n", "<leader>lt", function()
+			toggle_telescope(harpoon:list())
+		end)
+		vim.keymap.set("n", "<leader>l", function()
+			harpoon.ui:toggle_quick_menu(harpoon:list())
+		end)
 	end,
 }
