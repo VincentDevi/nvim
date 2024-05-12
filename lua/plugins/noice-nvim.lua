3eb6c50a77259fb80e9c13caa80c1836c666ca7a
Vincent Devilers <vincent.devilers@gmail.com>
2024-05-12 21:20:59 +0200

    delete preset from base config

--- lua/plugins/noice-nvim.lua
+++ lua/plugins/noice-nvim.lua
@@ -1,9 +1,7 @@
 return {
 	"folke/noice.nvim",
 	event = "VeryLazy",
-	opts = {
-		-- add any options here
-	},
+	opts = {},
 	dependencies = {
 		-- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
 		"MunifTanjim/nui.nvim",
@@ -22,13 +20,10 @@ return {
 					["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
 				},
 			},
-			-- you can enable a preset for easier configuration
-			presets = {
-				bottom_search = true, -- use a classic bottom cmdline for search
-				command_palette = true, -- position the cmdline and popupmenu together
-				long_message_to_split = true, -- long messages will be sent to a split
-				inc_rename = false, -- enables an input dialog for inc-rename.nvim
-				lsp_doc_border = false, -- add a border to hover docs and signature help
+			cmdline = {
+				enabled = true, -- enables the Noice cmdline UI
+				view = "cmdline_popup", -- view for rendering the cmdline. Change to `cmdline` to get a classic cmdline at the bottom
+				opts = {}, -- global options for the cmdline. See section on views
 			},
 		})
 	end,
